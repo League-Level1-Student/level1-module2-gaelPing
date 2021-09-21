6f822deaed6427efa97e13dd1a716a4260204eb9
root <root@2df902153ea7>
2021-09-21 01:32:24 +0000

    3e

---
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,11 @@
+package _01_sea_creature;
+
+public class SeaCreatureRunner {
+public static void main(String[] args) {
+	SeaCreature Spongebob = new SeaCreature("spongebob");
+	
+	Spongebob.getName();
+	
+	
+}
+}
